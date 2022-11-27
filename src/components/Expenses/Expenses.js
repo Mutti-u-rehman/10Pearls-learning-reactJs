e09686f08fce8fcb3365a35888d44503710e82c8
Mutti u rehman <mutti.mughal@gmail.com>
2022-11-27 16:15:59 +0500

    learning: adding dynamic styles and add new card UI

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 import ExpensesList from "./ExpensesList";
 import Card from "../UI/Card";
 import ExpensesFilter from "./ExpensesFilter";
+import ExpensesChart from './ExpensesChart';
 import "./Expenses.css";
 
 const Expenses = (props) => {
@@ -22,7 +23,7 @@ const Expenses = (props) => {
         <ExpensesFilter
           selected={filteredYear}
           onChangeFilter={onChangeFilterHandler}
-        />
+        /><ExpensesChart expenses={filteredExpenses} />
         <ExpensesList items={ filteredExpenses }/>
       </Card>
     </li>
