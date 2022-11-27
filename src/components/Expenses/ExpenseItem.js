cdc96ff2ac81018c13436d8ea0af2f3e0187d584
Mutti u rehman <mutti.mughal@gmail.com>
2022-11-27 12:44:31 +0500

    learning: lifting the state up

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -7,7 +7,6 @@ import './ExpenseItem.css';
 const ExpenseItem = (props) => {
 
   const [title, setTitle] = useState(props.title);
-  console.log("testing")
 
   const clickHandler = () => {
     setTitle("Update title");
