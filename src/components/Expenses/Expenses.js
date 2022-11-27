4ce753be7534b63ca59d432e9f929f1d9b66a035
Mutti u rehman <mutti.mughal@gmail.com>
2022-11-27 15:08:37 +0500

    learning: conditionaly content rendering based on filtered content

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -12,20 +12,30 @@ const Expenses = (props) => {
     setFilteredYear(selectedYear);
   };
 
+  const filteredExpenses = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+
+  let expenseContent = <p>No Expenses Found.</p>;
+
+  if(filteredExpenses.length > 0) {
+    expenseContent = filteredExpenses.map((expense) => (
+      <ExpenseItem
+        key = {expense.id}
+        title={expense.title}
+        amount={expense.amount}
+        date={expense.date}
+      />
+    ))
+  }
+
   return (
     <Card className="expenses">
       <ExpensesFilter
         selected={filteredYear}
         onChangeFilter={onChangeFilterHandler}
       />
-      {props.items.map((expense) => (
-        <ExpenseItem
-          title={expense.title}
-          amount={expense.amount}
-          date={expense.date}
-        />
-      ))}
-      
+      { expenseContent }
     </Card>
   );
 };
